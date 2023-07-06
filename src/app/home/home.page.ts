504c120a16064a848172c8a978312e75add04b3e
KaviCCT <sba22391@student.cct.ie>
2023-07-06 14:21:11 +0100

    Pre tabs and routing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
+import { environment } from 'src/environments/environment';
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public httpClient:HttpClient) {
+    this.loadData()
+  }
 
-  constructor() {}
-
+  loadData(){
+      this.httpClient.get(`${API_URL}/top-headlines?country=ie&apiKey=${API_KEY}`).subscribe(results =>{
+        console.log(results);
+    })
+  }
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'https://newsapi.org/v2/',
+  API_KEY: '466e4ac6b8844da49bb2869f3b787ed6'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
